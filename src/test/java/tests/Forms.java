1f788e0dd26030d3311a5aa546151f90eaae5240
Alex <2231790@gmail.com>
2025-07-12 22:10:06 +0300

    add studentRegistrationFormTest (home-work3)

--- src/test/java/tests/Forms.java
+++ src/test/java/tests/Forms.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Forms {
@@ -26,36 +28,35 @@ public class Forms {
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("2@mail.ru");
-        $("#gender-radio-1").click();
-        $("#userNumber").setValue("89031234567");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("8903123456");
         $("#dateOfBirthInput").click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("1995");
         $(".react-datepicker__month-select").selectOption("March");
-        $(".react-datepicker__day react-datepicker__day--015").click();
+        $(".react-datepicker__day.react-datepicker__day--015").click();
         $("#subjectsInput").setValue("History").pressEnter(); // это не понятно
         $("#subjectsInput").setValue("English").pressEnter(); // это не понятно
-        $("#hobbies-checkbox-1").click();
-        $("#hobbies-checkbox-2").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFile(file);
         $("#currentAddress").setValue("Some address");
         $("#react-select-3-input").setValue("NCR").pressEnter(); // это не понятно
         $("#react-select-4-input").setValue("Delhi").pressEnter(); // это не понятно
-
-
-        $("#submit").scrollIntoView(true).click();
         $("#submit").click();
 
         $(".table-responsive").shouldHave(
-                text("Alex Surname Ivanov"),
-                text("2@name.ru"),
+                text("Alex Ivanov"),
+                text("2@mail.ru"),
                 text("Male"),
-                text("89031234567"),
-                text("15 March,1975"),
+                text("8903123456"),
+                text("15 March,1995"),
                 text("History, English"),
                 text("Sports, Reading"),
                 text("file.png"),
                 text("Some address"),
                 text("NCR Delhi"));
+
+//        $("#closeLargeModal").shouldBe(Condition.visible).click();
     }
 }
